c1d750517e3f26a98c7d6ea19e439dcac338885c
Just-iiiiiiiiiiiiiin <wyslijtokajaki@gmail.com>
2022-01-24 09:35:31 +0100

    a

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main() {
 
     vector <int> wektor;
+    double suma=0;
+    int x;
+    double arytme=0;
     cout << "Podaj ile elementow" << endl;
     int elementy =0;
     cin >> elementy;
@@ -17,8 +20,20 @@ int main() {
     }
 
     for (int i =0; i < elementy; i++) {
-        cout << "Element: " << i << ": " << wektor[i] << endl;
+        cout << "Element: " << i << ": " << wektor[i] << endl;//wektor[i] nie jest to tablica ale zmienna dynamiczna która sama siebie usuwa
     }
+
+    /*for(int x : wektor)//pętla foreach nie działa teraz ale może kiedyś zadziała
+        cout << "Element: " << x << ": " << wektor[x] << endl;*/
+
+    for (int i =0; i < elementy; i++){
+        suma += wektor[i];
+        x=i;}
+
+    arytme = suma;
+
+    cout << suma << endl;
+    cout << arytme/x;
 
     return 0;
 }
