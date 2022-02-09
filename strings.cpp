09337a50bd73a3a81f38774f79904e9ff87d1bbb
Kajakaletakifajny <wyslijtokajaki@gmail.com>
2022-02-09 09:30:16 +0100

    a

---
+++ strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>//klasa obiektów dynamicznych skalowalnych
+
+using namespace std;
+int main()
+{
+    string napis;
+    //getline(cin,napis); //sposób na pobieranie łańcuchów wieloczłonowych
+    napis = "bob jest bobem";
+    napis.append(" ale fajnym"); //dołączamy na końcu
+    napis.insert(3,"ak"); //dołączamy gdzie chcemy
+    cout <<napis.compare("bobak jest bobem ale fajnym")<<endl;//zwraca zero dla napisów takich samych
+    cout << napis;                                            // lub liczbe rozna od zero dla roznych
+
+    cout << napis;
+
+    return 0;
+}
