0a132a091f7bc1e39fb16918b8f9acb4699d183b
Just-iiiiiiiiiiiiiin <wyslijtokajaki@gmail.com>
2022-03-17 10:54:58 +0100

    a

--- c_union.cpp
+++ c_union.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
 using namespace std;
+
+union Student{
+    int imie;
+    int ocena;
+};
+
 int main()
 {
-    int ocena;
-    string imie;
+    Student uczen;
+    uczen.ocena = 4;
+    uczen.imie = 5;
+    cout << sizeof(Student) << endl;
+    cout << uczen.ocena; //wypisze 5 bo jest ostania wpisana do union
 
     return 0;
 }
