cab1120b2c0390065113ec81c10779e1f64963ed
Just-iiiiiiiiiiiiiin <wyslijtokajaki@gmail.com>
2022-05-23 09:28:41 +0200

    a

--- strtol.cpp
+++ strtol.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     const char *napisI = "3";
     const char *napisF = "3.14159";
-    /*cout << atol(napisI) << endl;
+    cout << atol(napisI) << endl;
     cout << strtol(napisI, NULL, 0) << endl;
     cout << atof(napisF) << endl;
     cout << strtod(napisF, NULL) << endl;
-    cout << strtof(napisF, NULL) << endl;*/
+    cout << strtof(napisF, NULL) << endl;
     char *wsk;
     cout << "strtol: " << strtol(napisF, &wsk, 0) <<endl;
     cout << "wsk: " << wsk << endl;
